d8e0cd4ca7edf259285d0c5f77ad9c3d5c0da4e3
Joe Henriod <joehenriod@gmail.com>
2015-11-02 09:08:04 +0000

    implement require authorized for current section

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,23 +1,23 @@
 class Instructor::LessonsController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
 
-   def new
-    if current_section.course.user != current_user
-      return render :text => 'Unauthorized', :status => :unauthorized
-    end
+  def new
     @lesson = Lesson.new
   end
 
   def create
-    @section = current_section
-      if @section.course.user != current_user
-      return render :text => 'Unauthorized', :status => :unauthorized
-      end
-    @lesson = @section.lessons.create(lesson_params)
-    redirect_to instructor_course_path(@section.course)
+    @lesson = current_section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(current_section.course)
   end
 
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render :text => 'Unauthorized', :status => :unauthorized
+    end
+  end
 
   helper_method :current_section
   def current_section
@@ -27,5 +27,4 @@ class Instructor::LessonsController < ApplicationController
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
   end
-
 end
