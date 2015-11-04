840eb2daad69c795f17ff85c961282e2a2e00397
Joe Henriod <joehenriod@gmail.com>
2015-11-04 18:18:07 +0000

    User Registration Upgrade

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,6 +2,7 @@ class EnrollmentsController < ApplicationController
 
 	def create
 		current_user.enrollments.create(:course => current_course)
+		redirect_to course_path(current_course)
 	end
 
 	private
