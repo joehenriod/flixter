6f413985ed31d62fd2dafd61b8788bfb739cc325
Joe Henriod <joehenriod@gmail.com>
2015-11-05 06:27:26 +0000

    deleted hanging code

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,7 @@ class User < ActiveRecord::Base
 
    def enrolled_in?(course)
     return enrolled_courses.include?(course)
-  end
+   end
 	    
-	    return enrolled_courses.include?(course)
-
-	end
 
 end
