af97d4b39843036855e3df796ae037b443f41f4b
Joe Henriod <joehenriod@gmail.com>
2015-11-05 05:52:44 +0000

    Logic for User Enrollments

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   def enrolled_in?(course)
+	   	enrolled_courses = []
+	    enrollments.each do |enrollment|
+	      enrolled_courses << enrollment.course
+	    end
+	end
+	
 end
