2f3469000c9f8ea65c5d2735e7c8b011c7a1146f
Joe Henriod <joehenriod@gmail.com>
2015-11-06 02:21:06 +0000

    Enrollments Controller

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,8 +1,28 @@
 class EnrollmentsController < ApplicationController
+	before_action :authenticate_user!
 
 	def create
 		current_user.enrollments.create(:course => current_course)
 		redirect_to course_path(current_course)
+
+		# Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      :email => 'example@stripe.com',
+      :card  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
 	end
 
 	private
