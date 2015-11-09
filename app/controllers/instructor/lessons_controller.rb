ceac017804210790ac68de4c03cf9b4bac29bbf5
Joe Henriod <joehenriod@gmail.com>
2015-11-09 05:58:46 +0000

    update added to lesson controller

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -11,6 +11,10 @@ class Instructor::LessonsController < ApplicationController
     redirect_to instructor_course_path(current_section.course)
   end
 
+  def update
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+
   private
 
   def require_authorized_for_current_section
