a682cc21098907c64e758d70fc42b87ea70d6216
Joe Henriod <joehenriod@gmail.com>
2015-11-15 06:44:03 +0000

    Static careers page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Flixter::Application.routes.draw do
   resource :dashboard, :only => [:show]
   root 'static_pages#index'
   get 'privacy', :to => 'static_pages#privacy'
+  get 'careers', :to => 'static_pages#careers'
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => :create
   end
